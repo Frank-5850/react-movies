32afe15184b93bc4eac1840b206c8d80930b4995
Franco <phanfranco@yahoo.com>
2021-04-20 01:17:56 -0700

    Added state to app and created componentDidMount method to fetch movies and setStates.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,34 @@ import { getMoviesByName, getMoviesByID } from "./Utils/API";
 import MovieCard from "./Components/MovieCard";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "batman",
+      isLoading: false,
+      movies: [],
+      error: null,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const movieSearch = await getMoviesByName(this.state.search);
+      this.setState({
+        movies: movieSearch,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: error,
+        movies: [],
+        isLoading: false,
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,19 +1,18 @@
 const apiKey = process.env.REACT_APP_APIKEY;
 const baseUrl = "http://www.omdbapi.com/";
 
-const getMoviesByName = async (search) => {
-  const url = `${baseUrl}?apikey=${apiKey}&s=batman`;
+export const getMoviesByName = async (search) => {
+  const url = `${baseUrl}?apikey=${apiKey}&s=${search}`;
   try {
     const results = await fetch(url);
     const data = await results.json();
-    console.log("data", data);
     return data.Search;
   } catch (error) {
     console.log(error);
   }
 };
 
-const getMoviesByID = async (movieId) => {
+export const getMoviesByID = async (movieId) => {
   const url = `${baseUrl}?apikey=${apiKey}&i=tt1285016 `;
   try {
     const results = await fetch(url);
@@ -24,8 +23,3 @@ const getMoviesByID = async (movieId) => {
     console.log(error);
   }
 };
-
-getMoviesByName();
-getMoviesByID();
-
-export default { getMoviesByName, getMoviesByID };
