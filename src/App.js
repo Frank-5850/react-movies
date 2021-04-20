bcc40291ecc234306396e8573095431cfc7d1e8f
Franco <phanfranco@yahoo.com>
2021-04-20 01:22:54 -0700

    Mapped through array of movies state to render each movies poster, title, and type passed down as props.

--- src/App.js
+++ src/App.js
@@ -35,11 +35,13 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <MovieCard
-          title="batman"
-          type="movie"
-          posterUrl="https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg"
-        />
+        {this.state.movies.map((movies) => (
+          <MovieCard
+            title={movies.Title}
+            type={movies.Type}
+            posterUrl={movies.Poster}
+          />
+        ))}
       </div>
     );
   }
